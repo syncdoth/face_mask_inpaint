640d73043f6060a52eeadef32972f313db63bb90
syncdoth <choisehyun98@gmail.com>
2021-11-09 13:02:43 +0900

    add skeleton

---
+++ modules/mask_detector.py
@@ -0,0 +1,11 @@
+import torch
+from torch import nn
+
+
+class MaskDetector(nn.Module):
+
+    def __init__(self, params):
+        super().__init__()
+
+    def forward(self, image):
+        pass
---
+++ modules/model.py
@@ -0,0 +1,44 @@
+import torch
+from torch import nn
+
+from example_guided_att import ExampleGuidedAttention
+from mask_detector import MaskDetector
+
+
+class ImageEncoder(nn.Module):
+
+    def __init__(self, params):
+        super().__init__()
+
+    def forward(self, image, mask=None):
+        pass
+
+
+class ImageDecoder(nn.Module):
+
+    def __init__(self, params):
+        super().__init__()
+
+    def forward(self, encoded_features):
+        pass
+
+
+class ReferenceFill(nn.Module):
+
+    def __init__(self, mask_params, encoder_params, decoder_params):
+        super().__init__()
+        self.mask_detector = MaskDetector(mask_params)
+        self.src_encoder = ImageEncoder(encoder_params)
+        self.ref_encoder = ImageEncoder(encoder_params)
+        self.decoder = ImageDecoder(decoder_params)
+
+        self.attention = ExampleGuidedAttention(encoder_params.out_channels)
+
+    def forward(self, src_image, ref_image):
+        src_mask = self.mask_detector(src_image)
+        src_features = self.src_encoder(src_image, src_mask)
+        ref_features = self.ref_encoder(ref_image)
+
+        enc_features = self.attention(src_mask, src_features, ref_features)
+        dec_image = self.decoder(enc_features)
+        return dec_image
